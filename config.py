5ab80963e2e9e70ba103278edde9efee6b7a3cfa
Alex <algoar@fp.injoaquimmir.cat>
2024-02-14 18:46:21 +0100

    Cambiar nombre a variable de db type

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ class Config:
     SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
     DEBUG = environ.get('DEBUG', False)
 
-    DATABASE_TYPE = environ.get('DATABASE_TYPE')
-    if DATABASE_TYPE == 'mysql':
+    DB_TYPE = environ.get('DB_TYPE')
+    if DB_TYPE == 'mysql':
         DB_USER = environ.get('DB_USER')
         DB_PASSWORD = environ.get('DB_PASSWORD')
         DB_HOST = environ.get('DB_HOST')
         DB_NAME = environ.get('DB_NAME')
         DB_PORT = environ.get('DB_PORT', '3306')
         SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-    elif DATABASE_TYPE == 'sqlite':
+    elif DB_TYPE == 'sqlite':
         SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(basedir, environ.get('SQLITE_FILE_RELATIVE_PATH'))
     else:
         raise ValueError("Tipo de base de datos no soportado")
